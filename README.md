119d42717ae185212982caa151153addccd86c06
Dirk Rejahl <dirk@digiglu.co.uk>
2019-03-11 14:30:17 +0000

    wip

---
+++ README.md
@@ -0,0 +1,1 @@
+# golang-json-schema-ref-resolver
---
+++ resolve-cli.go
@@ -0,0 +1,7 @@
+package main
+
+import resolve "github.com/DigiGlu/golang-json-schema-ref-resolver/resolver"
+
+func main() {
+	resolve.SayHello()
+}
