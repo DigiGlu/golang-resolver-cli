716b0b9328942dd4893a04ee8acd7f94aa3e765d
Dirk Rejahl <dirk@digiglu.co.uk>
2019-03-12 08:43:54 +0000

    wip

--- resolve-cli.go
+++ resolve-cli.go
@@ -1,7 +1,23 @@
 package main
 
-import resolve "github.com/DigiGlu/golang-json-schema-ref-resolver/resolver"
+// import resolver "github.com/DigiGlu/golang-json-schema-ref-resolver/resolver"
+import resolver "../golang-json-schema-ref-resolver/resolver"
 
 func main() {
-	resolve.SayHello()
+	// inJSON := []byte(`{"person": { "properties": { "firstName": { "type": "string" }, "lastName": { "type": "string" }}}, "employee": { "properties": { "$ref": "#/person/properties", "salary": { "type": "number" }}}}`)
+
+	var jsonBytes = []byte(`
+	{
+		"key1":{
+			"Item1": "Value1",
+			"Item2": 10 },
+		"key2":{
+			"Item1": "Value2",
+			"Item2": 20 },
+		"key3":{
+			"Item1": "Value3",
+			"Item2": 4711 }
+	}`)
+
+	resolver.Dereference(jsonBytes)
 }
